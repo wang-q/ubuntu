70f34d5b763a356b4a856bda13b39948c408af02
wang-q <wang-q@outlook.com>
2019-08-02 02:56:32 +0800

    Moved from egavm/

---
+++ prepare/1-apt.sh
@@ -0,0 +1,86 @@
+#!/usr/bin/env bash
+
+echo "====> Install softwares via apt-get <===="
+
+echo "==> Disabling the release upgrader"
+sudo sed -i.bak 's/^Prompt=.*$/Prompt=never/' /etc/update-manager/release-upgrades
+
+echo "==> Switch to the USTC mirror"
+
+# https://lug.ustc.edu.cn/wiki/mirrors/help/ubuntu
+cat <<EOF > list.tmp
+# USTC MIRRORS
+deb https://mirrors.ustc.edu.cn/ubuntu/ bionic main restricted universe multiverse
+deb-src https://mirrors.ustc.edu.cn/ubuntu/ bionic main restricted universe multiverse
+
+deb https://mirrors.ustc.edu.cn/ubuntu/ bionic-security main restricted universe multiverse
+deb-src https://mirrors.ustc.edu.cn/ubuntu/ bionic-security main restricted universe multiverse
+
+deb https://mirrors.ustc.edu.cn/ubuntu/ bionic-updates main restricted universe multiverse
+deb-src https://mirrors.ustc.edu.cn/ubuntu/ bionic-updates main restricted universe multiverse
+
+deb https://mirrors.ustc.edu.cn/ubuntu/ bionic-backports main restricted universe multiverse
+deb-src https://mirrors.ustc.edu.cn/ubuntu/ bionic-backports main restricted universe multiverse
+
+## Not recommended
+# deb https://mirrors.ustc.edu.cn/ubuntu/ bionic-proposed main restricted universe multiverse
+# deb-src https://mirrors.ustc.edu.cn/ubuntu/ bionic-proposed main restricted universe multiverse
+
+EOF
+
+if [ ! -e /etc/apt/sources.list.bak ]; then
+    sudo cp /etc/apt/sources.list /etc/apt/sources.list.bak
+fi
+sudo mv list.tmp /etc/apt/sources.list
+
+# Virtual machines needn't this and I want life easier.
+# https://help.ubuntu.com/lts/serverguide/apparmor.html
+if [ "$(whoami)" == 'vagrant' ]; then
+    echo "==> Disable AppArmor"
+    sudo service apparmor stop
+    sudo update-rc.d -f apparmor remove
+fi
+
+echo "==> Disable whoopsie"
+sudo sed -i 's/report_crashes=true/report_crashes=false/' /etc/default/whoopsie
+sudo service whoopsie stop
+
+echo "==> Install linuxbrew dependences"
+sudo apt-get -y update
+sudo apt-get -y upgrade
+sudo apt-get -y install build-essential curl git m4 ruby texinfo
+sudo apt-get -y install libbz2-dev zlib1g-dev
+sudo apt-get -y install libcurl4-openssl-dev libexpat-dev libncurses-dev
+
+echo "==> Install other software"
+sudo apt-get -y install aptitude gawk csh parallel vim graphviz screen unzip xsltproc numactl
+
+echo "==> Install develop libraries"
+sudo apt-get -y install libdb-dev libreadline-dev libedit-dev
+sudo apt-get -y install libgd-dev libxml2-dev
+sudo apt-get -y install libgsl0ldbl libgsl0-dev
+
+# Gtk stuff, Need by alignDB
+# install them in a fresh machine to avoid problems
+echo "==> Install gtk3"
+sudo apt-get -y install libcairo2-dev libglib2.0-0 libglib2.0-dev libgtk-3-dev libgirepository1.0-dev
+sudo apt-get -y install gir1.2-glib-2.0 gir1.2-gtk-3.0 gir1.2-webkit-3.0
+
+echo "==> Install gtk3 related tools"
+sudo apt-get -y install xvfb glade
+
+#echo "==> Install nautilus plugins"
+#sudo apt-get -y install nautilus-open-terminal nautilus-actions
+
+# Mysql will be installed separately.
+# Remove system provided mysql package to avoid confusing linuxbrew.
+echo "==> Remove system provided mysql"
+sudo apt-get -y purge mysql-common
+
+echo "==> Restore original sources.list"
+if [ -e /etc/apt/sources.list.bak ]; then
+    sudo rm /etc/apt/sources.list
+    sudo mv /etc/apt/sources.list.bak /etc/apt/sources.list
+fi
+
+echo "====> Basic software installation complete! <===="
